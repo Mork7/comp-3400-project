0ecea36be21402ec2a5156e748135b8979892847
Mark Finerty <finertymark@gmail.com>
2025-02-26 16:22:57 -0500

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,11 +5,10 @@ using namespace std;
 
 int main(){
 
-     
     Date discharge(2025, 2, 26);
 
     Patient p1("John Doe","5199353845", Date(1995,7,16), "Flu", "Intravenis Fluids");
-    Patient p2("Mork Fin","5199034721", Date(1992,07,21), "Elephant Penis", "Penis reduction surgery");
+    Patient p2("Mork Fin","5199284721", Date(1992,07,21), "Tinnitus", "Music Therapy");
     p1.discharge(discharge);
     p1.displayInfo();
     p2.displayInfo();
